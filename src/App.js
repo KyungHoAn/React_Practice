940e2c2f2e59c3486498fe1539310afc0341244e
kyungho <akh1216@gmail.com>
2022-09-24 18:14:20 +0900

    map활용_9.24

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import ForEach from './R014_ForEach'
+import React from "react";
+import "./App.css";
+import Map from "./R015_Map";
 
 function App() {
   return (
     <div>
       <h1>Start React Conponent</h1>
       <p>CSS :D</p>
-      <ForEach/>
+      <Map />
     </div>
   );
 }
