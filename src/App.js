2785434e1a8be2d2961202981b6195ce7b3044ba
kyungho <akh1216@gmail.com>
2022-09-25 23:00:42 +0900

    React_reactstrap_9.25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import PureComponentClass from "./R028_PureComponentClass";
+import ReactstrapAlerts from "./R034_ReactstapAlerts";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
       <h1>Start React Conponent</h1>
       <p>CSS :D</p>
-      <PureComponentClass />
+      <ReactstrapAlerts />
     </div>
   );
 }
