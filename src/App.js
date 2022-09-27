604d5c0b817b7860d46e9e82bf925d1ebc55002c
안경호 <akh1216@gmail.com>
2022-09-27 16:15:58 +0900

    Axios_Done_9.27

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import SweetAlert2Confirm from "./R058_Sweetalert2Confirm";
+import Promise from "./R065_Promise";
 
 function App() {
   return (
     <div>
       <h1>Start React Conponent</h1>
-      <SweetAlert2Confirm />
+      <Promise />
     </div>
   );
 }
