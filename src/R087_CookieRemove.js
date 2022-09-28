7ad9b2d72d0e2393dc38f7b52585e54fc464f138
kyungho <akh1216@gmail.com>
2022-09-28 23:30:25 +0900

    react-router-dom_9.28

--- src/R087_CookieRemove.js
+++ src/R087_CookieRemove.js
@@ -1,9 +1,29 @@
-import React,{Component} from "react";
-import cookie from 'react-cookies';
+import React, { Component } from "react";
+import cookie from "react-cookies";
 
 class R087_CookieRemove extends Component {
-    componentDidMount() {
-        const expires =  new Date();
-        
-    }   
+  componentDidMount() {
+    const expires = new Date();
+    expires.setMinutes(expires.getMinutes() + 60);
+    cookie.save("userid", "react200", {
+      path: "/",
+      expires,
+    });
+    setTimeout(function () {
+      cookie.remove("userid", { path: "/" });
+    }, 1000);
+    setTimeout(function () {
+      alert(cookie.load("userid"));
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <>
+        <h3>react-cookies remove</h3>
+      </>
+    );
+  }
 }
+
+export default R087_CookieRemove;
