03591ba8b9829ef203e83cd7445a7e974dcb0b87
안경호 <akh1216@gmail.com>
2022-10-01 23:19:58 +0900

    React_connect_node_10.1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,7 +13,14 @@ import Footer from './Footer/Footer';
 // login
 import LoginForm from './LoginForm';
 
-import reactProxy from './R109_reactProxy';
+import reactThrottle from './R095_reactThrottle';
+import floatingPopulationList from './Floating_population/floatingPopulationList';
+import rechartsSimpleLineChart from './Floating_population/rechartsSimpleLineChart';
+import floatingPopulationListChart from './Floating_population/floatingPopulationListChart';
+import floatingPopulationAreaChart from './Floating_population/floatingPopulationAreaChart';
+import floatingPopulationBarChart from './Floating_population/floatingPopulationBarChart';
+import floatingPopulationComposedChart from './Floating_population/floatingPopulationComposedChart';
+import floatingPopulationScatterChart from './Floating_population/floatingPopulationScatterChart';
 
 class App extends Component {
   render () {
@@ -21,7 +28,14 @@ class App extends Component {
       <div className="App">
         <HeaderAdmin/> 
         <Route exact path='/' component={LoginForm} />
-        <Route exact path='/reactProxy' component={reactProxy} />
+        <Route exact path='/Throttle' component={reactThrottle} />
+        <Route path='/floatPopulationList' component={floatingPopulationList} />
+        <Route path='/rechartsSimpleLineChart' component={rechartsSimpleLineChart} />
+        <Route path='/floatingPopulationListChart' component={floatingPopulationListChart} />
+        <Route path='/floatingPopulationAreaChart' component={floatingPopulationAreaChart} />
+        <Route path='/floatingPopulationBarChart' component={floatingPopulationBarChart} />
+        <Route path='/floatingPopulationComposedChart' component={floatingPopulationComposedChart} />
+        <Route path='/floatingPopulationScatterChart' component={floatingPopulationScatterChart} />
         <Footer/>
       </div>
     );
