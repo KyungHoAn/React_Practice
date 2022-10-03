80a103a3a16ba832abd7ecdaa6c5bd4ad9019e7d
안경호 <akh1216@gmail.com>
2022-10-03 18:48:43 +0900

    react_get/post_10.3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,6 +15,8 @@ import LoginForm from './LoginForm';
 
 import reactThrottle from './R095_reactThrottle';
 import reactProxy from './R109_reactProxy';
+import ApiGetJson from './R110_ApiGetJson';
+import ApiPostJson from './R111_ApiPostJson';
 import floatingPopulationList from './Floating_population/floatingPopulationList';
 import rechartsSimpleLineChart from './Floating_population/rechartsSimpleLineChart';
 import floatingPopulationListChart from './Floating_population/floatingPopulationListChart';
@@ -31,6 +33,8 @@ class App extends Component {
         <Route exact path='/' component={LoginForm} />
         <Route exact path='/Throttle' component={reactThrottle} />
         <Route exact path='/reactProxy' component={reactProxy} />
+        <Route exact path='/ApiGetJson' component={ApiGetJson}/>
+        <Route exact path='/ApiPostJson' component={ApiPostJson}/>
         <Route path='/floatPopulationList' component={floatingPopulationList} />
         <Route path='/rechartsSimpleLineChart' component={rechartsSimpleLineChart} />
         <Route path='/floatingPopulationListChart' component={floatingPopulationListChart} />
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ var app = express();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
-module.exports = app;
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Listening on port ${port}`));
