2347307d192ecb84ce17262520e7461edd0c43fd
안경호 <akh1216@gmail.com>
2022-10-13 10:36:25 +0900

    context_themeChange_10.13

---
+++ React_study/my-app/src/chapter_14/DarkOrLight.jsx
@@ -0,0 +1,24 @@
+import { useState, UseCallBack } from "react";
+import ThemeContext from "./ThemeContext";
+import MainContent from "./Maincontent";
+import { useCallback } from "react";
+
+function DarkOrLight(props) {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = useCallback(() => {
+        if(theme === "light") {
+            setTheme("dark");
+        } else if (theme === "dark"){
+            setTheme("light");
+        }
+    },[theme]);
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <MainContent/>
+        </ThemeContext.Provider>
+    );
+}
+
+export default DarkOrLight;
