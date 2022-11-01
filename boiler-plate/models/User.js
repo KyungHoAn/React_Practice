da0f641e7c9f2aadd720f37b57f42e2fb21f1d4a
kyungho <akh1216@gmail.com>
2022-11-01 23:17:54 +0900

    bcrypt_setting

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+// salt를 이용하여 비밀버호를 암호화
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,6 +33,31 @@ const userSchema = mongoose.Schema({
     //토큰 유효기간
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  // 비밀번호를 암호화 시킨다.
+  var user = this;
+
+  //비밀번호를 바꿀때만 암호화를 진행하기 위해서 조건문을 넣었다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+
+  // bcrypt.genSalt(saltRounds, function (err, salt) {
+  //   bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+  //     // Stroe hash in your password DB.
+  //   });
+  // });
+  // next()
 });
 
 const User = mongoose.model("User", userSchema);
