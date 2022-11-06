b7bf10696211d4002f26c33d1066f8bf82b12dc8
kyungho <akh1216@gmail.com>
2022-11-06 20:50:55 +0900

    login_auth

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser"); //server에서 분석해서 가져올 수 있게 해준다.
 const { User } = require("./models/User");
 
@@ -13,6 +13,7 @@ const config = require("./config/key");
 app.use(bodyParser.urlencoded({ extended: true }));
 // application / json 를 분석해서 가져올수 잇게 해준다.
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -40,30 +41,33 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (res, req) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne(
-    {
-      email: req.body.email,
-    },
-    (err, userInfo) => {
-      if(!userInfo) {
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
         return res.json({
           loginSuccess: false,
-          message: "제공된 이메일에 해당하는 유저가 없습니다."
-        })
-      }
-      //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
-      user.comparePassword(req.body.password, (err, isMatch)=> {
-        if(!isMatch)
-        return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
-      
-        //비밀번호까지 맞다면 토큰을 생성하기.
-        user.gernerateToken((err,user) => {
-          
-        })
-      })
+          message: "비밀번호가 틀렸습니다.",
+        });
 
-    }
-  ));
+      //비밀번호까지 맞다면 토큰을 생성하기.
+      user.gernerateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
 });
 
 app.listen(port, () => {
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 // salt를 이용하여 비밀버호를 암호화
 const userSchema = mongoose.Schema({
@@ -65,6 +66,21 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
   //plainPassword 1234567   암호화된 비밀번호
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+userSchema.methods.gernerateToken = function (cb) {
+  let user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+
+  let token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  // user._id + 'secretToken' = token
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
   });
 };
 
