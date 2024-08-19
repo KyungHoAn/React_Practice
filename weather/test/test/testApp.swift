d8b006c4a09b6df9e39a56c315c651108a302ce6
안경호 <akh1216@gmail.com>
2024-08-19 18:10:42 +0900

    React-Native prac

---
+++ weather/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 안경호 on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
