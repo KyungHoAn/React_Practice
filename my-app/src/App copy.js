9323d31247c74c5ec4d00506d6d0ab417f02f775
안경호 <akh1216@gmail.com>
2024-08-19 16:07:44 +0900

    movie api

---
+++ my-app/src/App copy.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []);  // useEffect : 한번만 동작
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' change")
+    }
+  }, [keyword]);  //keyword 가 변화할 때코드를 실행
+  useEffect(() => {
+    console.log("I run when 'counter' change")
+  }, [counter]);  //keyword 가 변화할 때코드를 실행
+  useEffect(() => {
+    console.log('I run when keyword and counter change')
+  },[keyword, counter])
+  
+  return (
+    <div>
+      <input value={keyword} onChange={onChange} type='text' placeholder="Search here ..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
